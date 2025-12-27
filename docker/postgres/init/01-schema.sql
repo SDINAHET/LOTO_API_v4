20bcf6a3880e76f80848f51cf8de8ee3408bc16b
SDINAHET <stephane.dinahet@gmail.com>
2025-12-27 05:56:30 +0100

    fix github action fix

--- docker/postgres/init/01-schema.sql
+++ docker/postgres/init/01-schema.sql
@@ -8,7 +8,7 @@ CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 --     enabled BOOLEAN DEFAULT TRUE
 -- );
 
-CREATE TABLE IF NOT EXISTS public.users (
+CREATE TABLE IF NOT EXISTS users (
   id text PRIMARY KEY,
   first_name text NOT NULL,
   last_name text NOT NULL,
--- docker/postgres/init/02_seed_user.sql
+++ docker/postgres/init/02_seed_user.sql
@@ -7,11 +7,11 @@
 -- )
 -- ON CONFLICT (email) DO NOTHING;
 
-INSERT INTO public.users (id, first_name, last_name, email, password, is_admin, created_at, updated_at)
+INSERT INTO users (id, first_name, last_name, email, password, is_admin, created_at, updated_at)
 VALUES (
   'ci-user-001',
   'CI',
-  'User',
+  'Admin',
   'test4@hbnb.com',
   '$2a$10$xeJc3OOp/XrF/s5oMZkM7u8cIzvaMLCezcWlI/NnTBNVDRQXrAaa2',
   true,
@@ -19,3 +19,16 @@ VALUES (
   to_char(now(), 'YYYY-MM-DD"T"HH24:MI:SS')
 )
 ON CONFLICT (email) DO NOTHING;
+
+-- INSERT INTO public.users (id, first_name, last_name, email, password, is_admin, created_at, updated_at)
+-- VALUES (
+--   'ci-user-001',
+--   'CI',
+--   'User',
+--   'test4@hbnb.com',
+--   '$2a$10$xeJc3OOp/XrF/s5oMZkM7u8cIzvaMLCezcWlI/NnTBNVDRQXrAaa2',
+--   false,
+--   to_char(now(), 'YYYY-MM-DD"T"HH24:MI:SS'),
+--   to_char(now(), 'YYYY-MM-DD"T"HH24:MI:SS')
+-- )
+-- ON CONFLICT (email) DO NOTHING;
